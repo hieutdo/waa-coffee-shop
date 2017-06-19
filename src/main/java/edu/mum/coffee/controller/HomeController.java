3dc651d1c36ecc099f4049b2fbb3a4e29eae122e
Hieu Do <mr.hieudt@gmail.com>
2017-06-18 21:24:51 -0500

    Create a product table and display it on home page

--- src/main/java/edu/mum/coffee/controller/HomeController.java
+++ src/main/java/edu/mum/coffee/controller/HomeController.java
@@ -1,13 +1,24 @@
 package edu.mum.coffee.controller;
 
+import edu.mum.coffee.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
 
+    private ProductService productService;
+
+    @Autowired
+    public HomeController(ProductService productService) {
+        this.productService = productService;
+    }
+
     @GetMapping({"/", "/index", "/home"})
-    public String homePage() {
+    public String homePage(Model model) {
+        model.addAttribute("products", productService.getAllProducts());
         return "index";
     }
 }
